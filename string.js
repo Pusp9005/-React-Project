9af6f72c190467db810c39786ecbb8acfa4c8d95
Pusp Raj Tripathi <pusprajtripathi2@gmail.com>
2024-04-05 11:30:53 +0530

    initial Commit

---
+++ string.js
@@ -0,0 +1,7 @@
+let str1 = "Mohan";
+let age =23;
+
+let str2 =str1 + " '" + age + " '" ;
+let str3=`${str1} '${age}'`
+console.log(str2);
+console.log(str3);
---
+++ variable.js
@@ -0,0 +1,15 @@
+if (true) {
+    var x = 5;
+    console.log(x);
+
+    let y = 4;
+    y =5;
+    const pi = 3.14;
+    // pi = 4;
+    const st={name:'PUSP RAJ TRIPATHI', age:20};
+    console.log(st);
+
+    st.name="Ram";
+    console.log(st);
+
+}
